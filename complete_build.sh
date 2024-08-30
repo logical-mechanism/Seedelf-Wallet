3322da4a3d9e3d6330292407c77c6c3e9a4f79a0
logicalmechanism <logical.mechanism@protonmail.com>
2024-08-29 22:33:56 -0700

    combining into a single validator and using new assist branch

--- complete_build.sh
+++ complete_build.sh
@@ -19,19 +19,10 @@ echo -e "\033[1;34m\nBuilding Contracts \033[0m"
 # keep the traces for testing if required
 aiken build --trace-level verbose --filter-traces all
 
-ran="acab"
-ran_cbor=$(python3 -c "import cbor2;hex_string='${ran}';data = bytes.fromhex(hex_string);encoded = cbor2.dumps(data);print(encoded.hex())")
-
-echo -e "\033[1;33m\nBuilding Pointer Contract \033[0m"
-aiken blueprint apply -o plutus.json -v pointer.params "${ran_cbor}"
-aiken blueprint convert -v pointer.params > contracts/pointer_contract.plutus
-cardano-cli conway transaction policyid --script-file contracts/pointer_contract.plutus > hashes/pointer.hash
-echo -e "\033[1;33m Pointer Contract Hash: $(cat hashes/pointer.hash) \033[0m"
-
-echo -e "\033[1;33m\nBuilding Wallet Contract \033[0m"
-aiken blueprint convert -v wallet.params > contracts/wallet_contract.plutus
-cardano-cli conway transaction policyid --script-file contracts/wallet_contract.plutus > hashes/wallet.hash
-echo -e "\033[1;33m Wallet Contract Hash: $(cat hashes/wallet.hash) \033[0m"
+echo -e "\033[1;33m\nBuilding Contract \033[0m"
+aiken blueprint convert -v seedelf.wallet.spend > contracts/seedelf_contract.plutus
+cardano-cli conway transaction policyid --script-file contracts/seedelf_contract.plutus > hashes/wallet.hash
+echo -e "\033[1;33m Contract Hash: $(cat hashes/wallet.hash) \033[0m"
 
 # end of build
 echo -e "\033[1;32m\nBuilding Complete! \033[0m"
