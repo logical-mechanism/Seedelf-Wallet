0ecc290b94d7eeb48e686d8fee922900b8453a64
logicalmechanism <logical.mechanism@protonmail.com>
2024-10-04 17:20:50 -0700

    should be much smaller contracts; off will need adjustment

--- complete_build.sh
+++ complete_build.sh
@@ -23,13 +23,18 @@ ran="05"
 ran_cbor=$(python3 -c "import cbor2;hex_string='${ran}';data = bytes.fromhex(hex_string);encoded = cbor2.dumps(data);print(encoded.hex())")
 
 echo -e "\033[1;33m\nBuilding Contract \033[0m"
-aiken blueprint apply -o plutus.json -v seedelf.contract.spend "${ran_cbor}"
-aiken blueprint convert -v seedelf.contract.spend > contracts/seedelf_contract.plutus
-cardano-cli conway transaction policyid --script-file contracts/seedelf_contract.plutus > hashes/seedelf.hash
-echo -e "\033[1;33m Seedelf Contract Hash: $(cat hashes/seedelf.hash) \033[0m"
+aiken blueprint apply -o plutus.json -v wallet.contract.spend "${ran_cbor}"
+aiken blueprint convert -v wallet.contract.spend > contracts/wallet_contract.plutus
+cardano-cli conway transaction policyid --script-file contracts/wallet_contract.plutus > hashes/wallet.hash
+echo -e "\033[1;33m Wallet Contract Hash: $(cat hashes/wallet.hash) \033[0m"
 
-aiken blueprint apply -o plutus.json -v seedelf.always_false.else "${ran_cbor}"
-aiken blueprint convert -v seedelf.always_false.else > contracts/always_false_contract.plutus
+aiken blueprint apply -o plutus.json -v seedelf.contract.mint "${ran_cbor}"
+aiken blueprint convert -v seedelf.contract.mint > contracts/seedelf_contract.plutus
+cardano-cli conway transaction policyid --script-file contracts/seedelf_contract.plutus > hashes/seedelf.hash
+echo -e "\033[1;33m Seedelf Token Hash: $(cat hashes/seedelf.hash) \033[0m"
+
+aiken blueprint apply -o plutus.json -v always_false.contract.else "${ran_cbor}"
+aiken blueprint convert -v always_false.contract.else > contracts/always_false_contract.plutus
 cardano-cli conway transaction policyid --script-file contracts/always_false_contract.plutus > hashes/always_false.hash
 echo -e "\033[1;33m Always False Contract Hash: $(cat hashes/always_false.hash) \033[0m"
 
