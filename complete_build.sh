30b2c12ef8d07f7cd35850c85cef6f3f3dde9b55
logicalmechanism <logical.mechanism@protonmail.com>
2024-11-22 17:15:50 -0800

    just reviewing everything, trying to make sure docs/comments/code is up to par

--- complete_build.sh
+++ complete_build.sh
@@ -13,7 +13,7 @@ rm hashes/* || true
 rm -fr build/ || true
 
 # compile the scripts with aiken build
-echo -e "\033[1;34m\nCompiling Contracts \033[0m"
+echo -e "\033[1;34m\nCompiling...\033[0m"
 
 # remove all traces
 aiken build --trace-level silent --trace-filter user-defined
@@ -23,26 +23,26 @@ aiken build --trace-level silent --trace-filter user-defined
 
 # some random string to make the contracts unique
 ran="acab"
-ran_cbor=$(python3 -c "import cbor2;encoded = cbor2.dumps(bytes.fromhex('${ran}'));print(encoded.hex())")
+ran_cbor=$(python3 -c "import cbor2; print(cbor2.dumps(bytes.fromhex('${ran}')).hex())")
 
 # build and apply parameters to each contract
-echo -e "\033[1;37m\nBuilding Wallet Contract \033[0m"
+echo -e "\033[1;37m\nBuilding Wallet Contract\033[0m"
 aiken blueprint apply -o plutus.json -m wallet "${ran_cbor}"
 aiken blueprint convert -m wallet > contracts/wallet_contract.plutus
 cardano-cli conway transaction policyid --script-file contracts/wallet_contract.plutus > hashes/wallet.hash
-echo -e "\033[1;33m Wallet Contract Hash: $(cat hashes/wallet.hash) \033[0m"
+echo -e "\033[1;33m Wallet Contract Hash: $(cat hashes/wallet.hash)\033[0m"
 
-echo -e "\033[1;37m\nBuilding Seedelf Contract \033[0m"
+echo -e "\033[1;37m\nBuilding Seedelf Contract\033[0m"
 aiken blueprint apply -o plutus.json -m seedelf "${ran_cbor}"
 aiken blueprint convert -m seedelf > contracts/seedelf_contract.plutus
 cardano-cli conway transaction policyid --script-file contracts/seedelf_contract.plutus > hashes/seedelf.hash
-echo -e "\033[1;33m Seedelf Contract Hash: $(cat hashes/seedelf.hash) \033[0m"
+echo -e "\033[1;33m Seedelf Contract Hash: $(cat hashes/seedelf.hash)\033[0m"
 
-echo -e "\033[1;37m\nBuilding Always False Contract \033[0m"
+echo -e "\033[1;37m\nBuilding Always False Contract\033[0m"
 aiken blueprint apply -o plutus.json -m always_false "${ran_cbor}"
 aiken blueprint convert -m always_false > contracts/always_false_contract.plutus
 cardano-cli conway transaction policyid --script-file contracts/always_false_contract.plutus > hashes/always_false.hash
-echo -e "\033[1;33m Always False Contract Hash: $(cat hashes/always_false.hash) \033[0m"
+echo -e "\033[1;33m Always False Contract Hash: $(cat hashes/always_false.hash)\033[0m"
 
 # end of build
-echo -e "\033[1;32m\nCompiling Complete! \033[0m"
+echo -e "\033[1;32m\nComplete!\033[0m"
