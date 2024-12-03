94bc20de9578ff59f22c7d91729871e93efb0118
logicalmechanism <logicalmechanism@protonmail.com>
2024-12-02 23:40:04 -0800

    adding in pallas modules

--- seedelf-cli/src/constants.rs
+++ seedelf-cli/src/constants.rs
@@ -1,5 +1,9 @@
-pub const WALLET_CONTRACT_ADDRESS: &str = "addr_test1wqe0jhwuala460p8e7ugh6xgchncvy357mdnz0h5mau95jg5n6p68";
+pub const WALLET_CONTRACT_HASH: &str = "32f95ddceffb5d3c27cfb88be8c8c5e7861234f6db313ef4df785a49";
 pub const SEEDELF_POLICY_ID: &str = "88176fa1b636ef39dd71d83b17fa487942492bb3af99fd375ec71847";
-pub const SCRIPT_REFERENCE_ADDRESS: &str = "addr_test1wr7cpae2kv94qmslc7na6g80sgx2vug9ekflrrezjfphy9clk4n65";
+pub const SCRIPT_REFERENCE_HASH: &str = "fd80f72ab30b506e1fc7a7dd20ef820ca67105cd93f18f2292437217";
+
 pub const WALLET_REFERENCE_UTXO: &str = "";
 pub const SEEDELF_REFERENCE_UTXO: &str = "";
+
+pub const PREPROD_WALLET_REFERENCE_UTXO: &str = "";
+pub const PREPROD_SEEDELF_REFERENCE_UTXO: &str = "";
