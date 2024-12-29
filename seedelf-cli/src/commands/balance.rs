40fb2a6045d3ca953e0bd9a1ebef6baf46a458f6
logicalmechanism <logicalmechanism@protonmail.com>
2024-12-28 23:12:06 -0800

    color all the text

--- seedelf-cli/src/commands/balance.rs
+++ seedelf-cli/src/commands/balance.rs
@@ -1,4 +1,5 @@
 use blstrs::Scalar;
+use colored::Colorize;
 use reqwest::Error;
 use seedelf_cli::display;
 use seedelf_cli::koios::UtxoResponse;
@@ -9,7 +10,7 @@ pub async fn run(network_flag: bool) -> Result<(), Error> {
     display::preprod_text(network_flag);
     display::block_number_and_time(network_flag).await;
 
-    println!("\nSeedelf Wallet Information:");
+    println!("{}", "\nSeedelf Wallet Information:".bright_white());
     
     let scalar: Scalar = setup::load_wallet();
     
@@ -18,13 +19,13 @@ pub async fn run(network_flag: bool) -> Result<(), Error> {
     let all_utxos: Vec<UtxoResponse> = utxos::collect_all_wallet_utxos(scalar, network_flag).await;
     let (total_lovelace, tokens) = utxos::assets_of(all_utxos.clone());
 
-    println!("\nWallet Has {} UTxOs", all_utxos.len());
-    println!("\nBalance: {:.6} ₳", total_lovelace as f64 / 1_000_000.0);
+    println!("\nWallet Has {} UTxOs", all_utxos.len().to_string().bright_yellow());
+    println!("\nBalance: {} ₳", format!("{:.6}", total_lovelace as f64 / 1_000_000.0).bright_yellow());
 
     if tokens.items.len() > 0 {
-        println!("\nTokens:\n");
+        println!("{}", "\nTokens:\n".bright_magenta());
         for asset in tokens.items.clone() {
-            println!("{} {}.{}", asset.amount, hex::encode(asset.policy_id.as_ref()), hex::encode(asset.token_name));
+            println!("{} {}.{}", asset.amount.to_string().white(), hex::encode(asset.policy_id.as_ref()).white(), hex::encode(asset.token_name).white());
         }
     }
 
--- seedelf-cli/src/commands/welcome.rs
+++ seedelf-cli/src/commands/welcome.rs
@@ -1,7 +1,9 @@
+use colored::Colorize;
+
 pub fn run() {
-    println!("\nWelcome to the seedelf-cli! 🌱🧝 A command line interface for the Seedelf wallet.");
-    println!("\nThe Seedelf wallet is a stealth wallet that hides the receiver and spender using a non-interactive variant of Schnorr's Σ-protocol for the Discrete Logarithm Relation.");
-    println!("\n");
-    println!("😀 Start your journey by creating a Seedelf with the cli command: create");
-    println!("\nYour wallet file will be stored in your home directory in the .seedelf folder 📁. Keep this file safe!");
+    println!("\n{} 🌱🧝 {}", "\nWelcome to seedelf-cli!".bright_white(), "A command line interface for the Seedelf stealth wallet protocol.".bright_yellow());
+    println!("{}", "\nThe Seedelf wallet is a stealth wallet protocol that hides the receiver and spender using a non-interactive variant of Schnorr's Σ-protocol for the Discrete Logarithm Relation.".bright_green());
+    println!("\n😀\n");
+    println!("{} {}", "Start your journey by creating a Seedelf with the cli command:".bright_purple(), "create".bright_cyan());
+    println!("\nYour wallet file will be stored in your home directory in the .seedelf folder.");
 }
