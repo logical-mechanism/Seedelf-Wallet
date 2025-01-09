7695dd4f8dfc20c7d56091f518820e7ef487940b
logicalmechanism <logicalmechanism@protonmail.com>
2025-01-09 00:54:43 -0800

    chore: adding requirement checks for contract build script

--- seedelf-contracts/complete_build.sh
+++ seedelf-contracts/complete_build.sh
@@ -1,6 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
+if command -v python3 &> /dev/null; then
+    echo -e "\033[1;35m python3 is installed and available on the PATH. \033[0m"
+else
+    echo -e "\033[1;31m python3 is not installed or not available on the PATH.\033[0m"
+    echo -e "\033[1;33m sudo apt install -y python3 \033[0m"
+    exit 1;
+fi
+
+if python3 -c "import cbor2" 2>/dev/null; then
+    echo -e "\033[1;35m cbor2 is installed and available for python3. \033[0m"
+else
+    echo -e "\033[1;31m cbor2 is not installed or not available for python3.\033[0m"
+    echo -e "\033[1;33m sudo apt-get install python3-cbor2 \033[0m"
+    exit 1;
+fi
+
+if command -v aiken &> /dev/null; then
+    echo -e "\033[1;35m aiken is installed and available on the PATH. \033[0m"
+else
+    echo -e "\033[1;31m aiken is not installed or not available on the PATH.\033[0m"
+    echo -e "\033[1;33m https://github.com/aiken-lang/aiken \033[0m"
+    exit 1;
+fi
+
+if command -v cardano-cli &> /dev/null; then
+    echo -e "\033[1;35m cardano-cli is installed and available on the PATH. \033[0m"
+else
+    echo -e "\033[1;31m cardano-cli is not installed or not available on the PATH.\033[0m"
+    echo -e "\033[1;33m https://github.com/IntersectMBO/cardano-cli \033[0m"
+    exit 1;
+fi
+
 # create directories if they dont exist
 mkdir -p contracts
 mkdir -p hashes
@@ -24,7 +56,7 @@ aiken build --trace-level silent --trace-filter user-defined
 # some random string to make the contracts unique
 rand="acabcafe"
 rand_cbor=$(python3 -c "import cbor2; print(cbor2.dumps(bytes.fromhex('${rand}')).hex())")
-echo "Random Seed:" ${rand}
+echo -e "\033[1;36m\nRandom Seed: ${rand} \033[0m"
 
 # build and apply parameters to each contract
 echo -e "\033[1;37m\nBuilding Wallet Contract\033[0m"
