67a79b8ad9845961b59bc0c5b204acc0ca58a204
logicalmechanism <logicalmechanism@protonmail.com>
2025-01-17 01:59:37 -0800

    slowly coming together

--- seedelf-cli/src/commands/dapp/newm/types.rs
+++ seedelf-cli/src/commands/dapp/newm/types.rs
@@ -1,4 +1,6 @@
+use seedelf_cli::koios::InlineDatum;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Hash, Clone, Default)]
 pub struct Wallet {
@@ -13,10 +15,56 @@ pub struct Token {
     pub amt: u64,
 }
 
+impl Token {
+    pub fn new(pid: String, tkn: String, amt: u64) -> Self {
+        Self { pid, tkn, amt }
+    }
+}
+
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Hash, Clone, Default)]
 pub struct SaleDatum {
     pub owner: Wallet,
     pub bundle: Token,
     pub cost: Token,
-    pub max_bundle_size: u64
+    pub max_bundle_size: u64,
+}
+
+pub fn extract_token(inline_datum: &Option<InlineDatum>, bundle_flag: bool) -> Option<Token> {
+    let position: usize = if bundle_flag {1} else {2};
+    inline_datum
+        .as_ref()
+        .and_then(|datum| match &datum.value {
+            Value::Object(value_map) => value_map.get("fields"),
+            _ => None,
+        })
+        .and_then(|fields| match fields {
+            Value::Array(fields) => fields.get(position), // Get the third element
+            _ => None,
+        })
+        .and_then(|field| match field {
+            Value::Object(field_map) => field_map.get("fields"),
+            _ => None,
+        })
+        .and_then(|fields| match fields {
+            Value::Array(fields) => {
+                // Extract the pid, tkn, and amt fields
+                let pid = fields.get(0).and_then(|field| match field {
+                    Value::Object(obj) => obj.get("bytes").and_then(|b| b.as_str().map(String::from)),
+                    _ => None,
+                })?;
+
+                let tkn = fields.get(1).and_then(|field| match field {
+                    Value::Object(obj) => obj.get("bytes").and_then(|b| b.as_str().map(String::from)),
+                    _ => None,
+                })?;
+
+                let amt = fields.get(2).and_then(|field| match field {
+                    Value::Object(obj) => obj.get("int").and_then(|i| i.as_u64()),
+                    _ => None,
+                })?;
+
+                Some(Token::new(pid, tkn, amt))
+            }
+            _ => None,
+        })
 }
--- seedelf-cli/src/commands/dapp/newm/view_sale.rs
+++ seedelf-cli/src/commands/dapp/newm/view_sale.rs
@@ -1,8 +1,9 @@
 use clap::Args;
 use colored::Colorize;
 use seedelf_cli::display::preprod_text;
-use crate::commands::dapp::newm::constants::{MAINNET_POINTER_POLICY_ID, PREPROD_POINTER_POLICY_ID};
-// use crate::commands::dapp::newm::types::SaleDatum;
+use crate::commands::dapp::newm::constants::{MAINNET_POINTER_POLICY_ID, PREPROD_POINTER_POLICY_ID, USE_USD_FLAG};
+use crate::commands::dapp::newm::types::extract_token;
+use seedelf_cli::utxos;
 
 use seedelf_cli::koios::{nft_utxo, UtxoResponse};
 /// Struct to hold command-specific arguments
@@ -32,8 +33,21 @@ pub async fn run(args: ViewSaleArgs, network_flag: bool) -> Result<(), String> {
                 return Err("No Sale Found".to_string());
             }
             let utxo: UtxoResponse = utxos.first().unwrap().clone();
-            let sale_datum = utxo.inline_datum.unwrap().value;
-            println!("{:?}", sale_datum);
+            let sale_datum = utxo.clone().inline_datum;
+            let bundle = extract_token(&sale_datum, true).unwrap();
+            let cost = extract_token(&sale_datum, false).unwrap();
+            
+            let (_sale_lovelace, tokens) = utxos::assets_of(vec![utxo.clone()]);
+            println!("{} {}", "\nTokens Left On Sale:".bright_magenta(), tokens.quantity_of(bundle.pid, bundle.tkn).unwrap());
+            if cost.pid == USE_USD_FLAG {
+                // Convert to f64 for floating-point division
+                let result = cost.amt as f64 / 1_000_000.0 / 1_000_000.0;
+                println!("{} {} {}", "Each Stream Token is".yellow(), result, "USD".yellow())
+            } else {
+                println!("{} {} {}", "Each Stream Token is".yellow(), format!("{:.6}", cost.amt as f64 / 1_000_000.0), "NEWM".yellow())
+
+            }
+
         }
         Err(err) => {
             eprintln!("Failed to fetch UTxOs: {}", err);
