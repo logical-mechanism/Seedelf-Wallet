4e383ddc6709a824e1f5a47b3d91cc4438815bd3
logicalmechanism <logicalmechanism@protonmail.com>
2025-02-04 12:12:36 -0800

    basic guide, need to review and make sure the flow makes sense

---
+++ seedelf-cli/src/commands/dapp/newm/guide.rs
@@ -0,0 +1,17 @@
+use colored::Colorize;
+use seedelf_cli::display;
+
+pub fn run(network_flag: bool) {
+    display::preprod_text(network_flag);
+    println!(
+        "{}",
+        "\nQuick How-To Guide For The NEWM dApp".bright_white(),
+    );
+    println!("\n 😀\n");
+    let url = if network_flag {"https://fan.square.newm.io/"} else {"https://marketplace.newm.io/"};
+    let flag = if network_flag {"--preprod"} else {""};
+    println!("View The Marketplace: {}", url.bright_cyan());
+    println!("\n{}", "Select which steaming tokens you wish to purchase. Locate the Pointer Asset Name in the information box on the song page and copy that text. The pointer asset name is used as the locator token for the create command.".bright_blue());
+    println!("\nRun: {} {}", "seedelf-cli dapp newm create -p POINTER_ASSET_NAME_HERE -a STREAM_TOKEN_AMOUNT_HERE".bright_magenta(), flag.bright_magenta());
+    println!("\n{}", "This will create an order for the stream tokens.".bright_green());
+}
--- seedelf-cli/src/commands/dapp/newm/mod.rs
+++ seedelf-cli/src/commands/dapp/newm/mod.rs
@@ -2,11 +2,13 @@ use clap::{Args, Subcommand};
 pub mod constants;
 pub mod types;
 pub mod view_sale;
+pub mod guide;
 
 #[derive(Subcommand)]
 pub enum NEWMCommands {
     /// View Sale Information
     View(view_sale::ViewSaleArgs),
+    Guide,
 }
 
 #[derive(Args)]
@@ -18,5 +20,6 @@ pub struct NEWMArgs {
 pub async fn run(args: NEWMArgs, preprod_flag: bool) -> Result<(), String> {
     match args.command {
         NEWMCommands::View(args) => view_sale::run(args, preprod_flag).await,
+        NEWMCommands::Guide => Ok(guide::run(preprod_flag)),
     }
 }
