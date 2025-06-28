8fa7a3e2cec7d7e6a9383f71bdedcc431ad5a165
logicalmechanism <logicalmechanism@protonmail.com>
2025-06-28 13:39:33 -0700

    adding comment about future work in balance

--- seedelf-cli/src/commands/balance.rs
+++ seedelf-cli/src/commands/balance.rs
@@ -25,6 +25,7 @@ pub async fn run(network_flag: bool, variant: u64) -> Result<(), Error> {
         "\nWallet Has {} UTxOs",
         all_utxos.len().to_string().bright_yellow()
     );
+    // we may want to hide this behind an optional flag later
     for utxo in all_utxos {
         println!("UTxO: {}#{}", utxo.tx_hash, utxo.tx_index);
     }
