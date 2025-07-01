3b462dd8f611f1dd8a38ba2d70bd2cc2b6e603e8
logicalmechanism <logicalmechanism@protonmail.com>
2025-07-01 13:07:19 -0700

    fmt, clippy, and tests pass

--- seedelf-cli/seedelf-cli/src/web_server.rs
+++ seedelf-cli/seedelf-cli/src/web_server.rs
@@ -1,8 +1,8 @@
 use colored::Colorize;
+use mime_guess::MimeGuess;
+use rust_embed::RustEmbed;
 use std::net::SocketAddr;
 use warp::{Filter, http::Response};
-use rust_embed::RustEmbed;
-use mime_guess::MimeGuess;
 
 #[derive(RustEmbed)]
 #[folder = "static/"]
@@ -30,7 +30,7 @@ pub async fn run_web_server(message: String, network_flag: bool) {
         let file = Asset::get("index.html").expect("index.html not found");
         let mut html = String::from_utf8(file.data.into_owned()).unwrap();
 
-        let dyn_msg = format!(r#"{{ "message": "{}" }}"#, message);
+        let dyn_msg = format!(r#"{{ "message": "{message}" }}"#);
         html = html.replace(r#"{ "message": "ACAB000000000000" }"#, &dyn_msg);
 
         let net_repl = if network_flag {
@@ -44,15 +44,15 @@ pub async fn run_web_server(message: String, network_flag: bool) {
     });
 
     // JS, CSS, and favicon routes
-    let js    = warp::path("index.js").map(|| {
+    let js = warp::path("index.js").map(|| {
         let file = Asset::get("index.js").expect("index.js not found");
         serve_bytes("index.js", file.data.into_owned())
     });
-    let css   = warp::path("index.css").map(|| {
+    let css = warp::path("index.css").map(|| {
         let file = Asset::get("index.css").expect("index.css not found");
         serve_bytes("index.css", file.data.into_owned())
     });
-    let ico   = warp::path("favicon.ico").map(|| {
+    let ico = warp::path("favicon.ico").map(|| {
         let file = Asset::get("favicon.ico").expect("favicon.ico not found");
         serve_bytes("favicon.ico", file.data.into_owned())
     });
@@ -61,13 +61,12 @@ pub async fn run_web_server(message: String, network_flag: bool) {
     let routes = html.or(js).or(css).or(ico);
 
     // Run server with graceful shutdown
-    let (_addr, server) = warp::serve(routes)
-        .bind_with_graceful_shutdown(addr, async {
-            tokio::signal::ctrl_c()
-                .await
-                .expect("Failed to install Ctrl-C handler");
-            println!("\n{}", "Shutdown signal received...".red());
-        });
+    let (_addr, server) = warp::serve(routes).bind_with_graceful_shutdown(addr, async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install Ctrl-C handler");
+        println!("\n{}", "Shutdown signal received...".red());
+    });
 
     server.await;
     println!("{}", "Server has stopped.".bright_purple());
